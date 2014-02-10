af4b6be4ee9fc1876a85763d643d9b1ee24e6982
Ari Weinstein <arixmail@gmail.com>
2014-02-09 21:08:04 -0500

    Updated tools

--- pbcopy.m
+++ pbcopy.m
@@ -1,27 +1,28 @@
 #import <UIKit/UIKit.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 
-int pbcopy() {
+int pbcopy(NSString *pasteboardType) {
     NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
     NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
-    NSString *inputString = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
     
-    if ([inputString hasPrefix:@"{\rtf"])
-        [[UIPasteboard generalPasteboard] setValue:inputString forPasteboardType:(NSString *)kUTTypeRTF];
-    else
-        [[UIPasteboard generalPasteboard] setString:inputString];
+    if (!pasteboardType)
+        pasteboardType = (NSString *)kUTTypeText;
     
-    [inputString release];
-    return !inputString;
+    [[UIPasteboard generalPasteboard] setData:inputData forPasteboardType:pasteboardType];
+    return !inputData;
 }
 
 int main(int argc, char **argv, char **envp) {
     @autoreleasepool {
         if (argc > 1) {
-            printf("Usage: %s [-help]\n%s takes the standard input and places it in the clipboard.\n", argv[0], argv[0]);
-            return 0;
+            if (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+                printf("Usage: %s [-help | public.pasteboard-type ]\n%s takes the standard input and places it in the clipboard. Optionally, the pasteboard type of the input may be specified.\n", argv[0], argv[0]);
+                return 0;
+            } else {
+                return pbcopy([NSString stringWithUTF8String:argv[1]]);
+            }
         } else {
-            return pbcopy();
+            return pbcopy(nil);
         }
     }
 }
--- pbpaste.m
+++ pbpaste.m
@@ -1,20 +1,38 @@
 #import <UIKit/UIKit.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 
-int pbpaste() {
-    NSString *pasteboardString = [[UIPasteboard generalPasteboard] string];
-    printf("%s", [pasteboardString UTF8String]);
-    
-    return !pasteboardString;
+int pbpaste(NSString *pasteboardType) {
+    UIPasteboard *clipboard = [UIPasteboard generalPasteboard];
+
+    if (![clipboard containsPasteboardTypes:@[pasteboardType]])
+        return -1;
+
+    NSData *data = [clipboard dataForPasteboardType:pasteboardType];
+    [[NSFileHandle fileHandleWithStandardOutput] writeData:data];
+
+    return 0;
 }
 
 int main(int argc, char **argv, char **envp) {
     @autoreleasepool {
         if (argc > 1) {
-            printf("Usage: %s [-help]\n%s takes the string from the clipboard and writes it to the standdard output.\n", argv[0], argv[0]);
-            return 0;
+            if (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+                printf("Usage: %s [-help | public.pasteboard-type ]\n%s takes the string from the clipboard and writes it to the standard output. Optionally, the pasteboard type to retrieve may be specified.\n", argv[0], argv[0]);
+                return 0;
+            } else {
+                return pbpaste([NSString stringWithUTF8String:argv[1]]);
+            }
         } else {
-            return pbpaste();
+            // UIPasteboardTypeListString is nil for some reason; no time to debug right now
+            NSArray *stringTypes = @[(id)kUTTypeText, (id)kUTTypePlainText, (id)kUTTypeUTF8PlainText];
+
+            int result;
+            for (NSString *type in stringTypes) {
+                result = pbpaste(type);
+                if (result == 0)
+                    break;
+            }
+            return result;
         }
     }
 }
